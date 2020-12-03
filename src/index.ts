48c8d455e952401bf3968a8be19741f5b570e386
Ivan Nikolić <niksy5@gmail.com>
2020-12-03 17:17:15 +0100

    Add support for custom languages

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import fs from "fs";
 import _ from "lodash";
-import * as path from "path";
 import peg from "pegjs";
-
-import ConversionsUtils from "./helpers/conversions";
+import ConversionsUtils, { IUnits } from "./helpers/conversions";
 
 export interface IRecipeResult {
   result?: {
@@ -20,19 +17,14 @@ export interface IRecipeResult {
 
 export enum LOGIC_OPERATOR {
   AND = "AND",
-  OR = "OR"
-}
-
-export enum LANGUAGES {
-  EN = "EN",
-  FR = "FR"
+  OR = "OR",
 }
 
 export enum UNKNOWN_REASONS {
   PARSING = "mismatch during parsing",
   PARSING_AMOUNT = "unknown amount",
   PARSING_UNIT = "unknown unit",
-  NO_ENTRY = "unavailable ingredient"
+  NO_ENTRY = "unavailable ingredient",
 }
 
 export interface IInputIngredient {
@@ -42,22 +34,25 @@ export interface IInputIngredient {
 }
 
 export default class RecipesParser {
-  private nlpParser: peg.Parser = peg.generate(
-    fs.readFileSync(
-      path.join(__dirname, `/../nlp/nlp-rules/rules_${this.langage}.pegjs`),
-      {
-        encoding: "utf8"
-      }
-    )
+  private nlpParser: peg.Parser = peg.generate(this.rules);
+
+  private conversionsUtils: ConversionsUtils = new ConversionsUtils(
+    this.units,
+    this.globalUnit
   );
-  constructor(private langage: "FR" | "EN") {}
+
+  constructor(
+    private rules: string,
+    private units: IUnits,
+    private globalUnit: any
+  ) {}
 
   public getIngredientsFromText(
     recipeInstructions: string[],
     returnUnitKey?: boolean
   ): IRecipeResult[] {
     const output: IRecipeResult = {
-      unknown: {}
+      unknown: {},
     };
 
     // Parse and normalize the ingredients
@@ -81,25 +76,22 @@ export default class RecipesParser {
 
         output.unknown = {
           instruction,
-          reasons: unknownReasons
+          reasons: unknownReasons,
         };
         return output;
       } else {
-        parts.amount = ConversionsUtils.normalizeAmount(
-          this.langage,
-          parts.amount
-        );
+        parts.amount = this.conversionsUtils.normalizeAmount(parts.amount);
 
         // get unit key match
         if (returnUnitKey) {
-          parts.unit = ConversionsUtils.getUnitKey(parts.unit);
+          parts.unit = this.conversionsUtils.getUnitKey(parts.unit);
         }
 
         output.result = {
           instruction,
           unit: parts.unit,
           amount: parts.amount,
-          ingredient: parts.ingredient
+          ingredient: parts.ingredient,
         };
         return output;
       }
